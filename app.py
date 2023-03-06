e29d0a5231df075950a9502e59bc9a6365ef95c8
vaibhav-soni-learnapp <125342282+vaibhav-soni-learnapp@users.noreply.github.com>
2023-03-06 21:52:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,28 @@ import requests
 from io import BytesIO
 
 # Step 2
-image_urls = [
-    "https://graphics-library-project.s3.amazonaws.com/graphic+library/cash-flow-statement/cashflow+(1).jpg",
-    "https://graphics-library-project.s3.amazonaws.com/graphic+library/cash-flow-statement/cashflow+(2).jpg",
-    "https://graphics-library-project.s3.amazonaws.com/graphic+library/cash-flow-statement/cashflow+(3).jpg"
-]
+course_images = {
+    "Course A": [
+        "https://graphics-library-project.s3.amazonaws.com/graphic+library/technical-analysis/technicalanalysis+(1).jpg",
+        "https://graphics-library-project.s3.amazonaws.com/graphic+library/technical-analysis/technicalanalysis+(2).jpg",
+        "https://graphics-library-project.s3.amazonaws.com/graphic+library/technical-analysis/technicalanalysis+(3).jpg"
+    ],
+    "Course B": [
+        "https://graphics-library-project.s3.amazonaws.com/graphic+library/boring-portfolio/boring+(1).jpg",
+        "https://graphics-library-project.s3.amazonaws.com/graphic+library/boring-portfolio/boring+(2).jpg",
+        "https://graphics-library-project.s3.amazonaws.com/graphic+library/boring-portfolio/boring+(3).jpg"
+    ]
+}
 
 # Step 4
-def display_image(url):
-    response = requests.get(url)
-    image_content = response.content
-    image = Image.open(BytesIO(image_content))
-    st.image(image, caption=url)
+def display_images(urls):
+    for url in urls:
+        response = requests.get(url)
+        image_content = response.content
+        image = Image.open(BytesIO(image_content))
+        st.image(image, caption=url)
 
 # Step 3
-selected_url = st.selectbox("Select an image URL:", image_urls)
-display_image(selected_url)
+selected_course = st.selectbox("Select a course:", list(course_images.keys()))
+selected_images = course_images[selected_course]
+display_images(selected_images)
